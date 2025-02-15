82f1c8bddb1295129aa0747f7b3ce1612e2728f9
Riyaz Qureshi <42235756+r4riyaz@users.noreply.github.com>
2025-02-15 23:23:36 +0530

    Add files via upload

---
+++ Scripts/kubenode.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+##Enable IPv4 packet forwarding on both nodes
+##https://kubernetes.io/docs/setup/production-environment/container-runtimes/#prerequisite-ipv4-forwarding-optional
+##It's a single command starting from cat to the 3rd line EOF, make sure copy and paste in one go.
+modprobe br_netfilter
+echo "br_netfilter" > /etc/modules-load.d/k8s.conf
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sudo sysctl --system
+sysctl net.ipv4.ip_forward
+sysctl net.bridge.bridge-nf-call-iptables
+
+
+##Install container runtime on all nodes
+sudo apt-get update
+sudo apt-get install containerd -y
+
+##Install Kubeadm, Kubelet and Kubectl on all nodes
+##https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
+
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+##Set the "systemd" cgroup driver for container runtime on all nodes
+##https://kubernetes.io/docs/setup/production-environment/container-runtimes/#containerd
+sudo mkdir -p /etc/containerd
+containerd config default | sed 's/SystemdCgroup = false/SystemdCgroup = true/g' | sudo tee /etc/containerd/config.toml
+sudo systemctl restart containerd
