1c16cbebe8507fa7f6c9cbdb36430664f09c4371
Riyaz Qureshi <42235756+r4riyaz@users.noreply.github.com>
2025-02-21 22:42:44 +0530

    Update kubemaster.sh

--- Scripts/kubemaster.sh
+++ Scripts/kubemaster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-##Enable IPv4 packet forwarding on both nodes
+echo "======= Enabling IPv4 packet forwarding ======="; sleep 2
 ##https://kubernetes.io/docs/setup/production-environment/container-runtimes/#prerequisite-ipv4-forwarding-optional
 ##It's a single command starting from cat to the 3rd line EOF, make sure copy and paste in one go.
 modprobe br_netfilter
@@ -17,11 +17,11 @@ sysctl net.ipv4.ip_forward
 sysctl net.bridge.bridge-nf-call-iptables
 
 
-##Install container runtime on all nodes
+echo "======= Installing container runtime ======="; sleep 2
 sudo apt-get update
 sudo apt-get install containerd -y
 
-##Install Kubeadm, Kubelet and Kubectl on all nodes
+echo "======= Installing Kubeadm, Kubelet and Kubectl ======="; sleep 2
 ##https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
 
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
@@ -32,14 +32,14 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
-##Set the "systemd" cgroup driver for container runtime on all nodes
+echo "======= Setting up the systemd cgroup driver for container runtime ======="; sleep 2
 ##https://kubernetes.io/docs/setup/production-environment/container-runtimes/#containerd
 sudo mkdir -p /etc/containerd
 containerd config default | sed 's/SystemdCgroup = false/SystemdCgroup = true/g' | sudo tee /etc/containerd/config.toml
 sudo systemctl restart containerd
 
 
-##Initialize control-plane node, 
+echo "======= Initializing control-plane node ======="; sleep 2 
 ##--ignore-preflight-errors='NumCPU,Mem' to ignore below errors related to Less CPUs Memory on EC2 Instance. You don't need to use "--ignore-preflight-errors='NumCPU,Mem'" it on local VM where we have CPUs 2 or more and memory 1700MB or more.
 ##[ERROR NumCPU]: the number of available CPUs 1 is less than the required 2
 ##[ERROR Mem]: the system RAM (957 MB) is less than the minimum 1700 MB
@@ -58,9 +58,10 @@ sudo chown root:root /root/.kube/config && sleep 20
 ##If we miss the token output from "kubeadmin init" command then we can run below command to get token command again. Then run it on Worker node.
 #kubeadm token create --print-join-command
 
-
-## Installing Network Addon
+echo "======= Installing Network Addon ======="; sleep 2
 ##https://kubernetes.io/docs/concepts/cluster-administration/addons/#networking-and-network-policy
 ##https://github.com/flannel-io/flannel#deploying-flannel-manually
 
 sudo kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+
+echo "====== K8s Master server initialization completed ======="
